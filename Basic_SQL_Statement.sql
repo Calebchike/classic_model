cdd4c6dfbfe785251ce946445cef7effe40f9c0c
Calebchike <chikecaleb6@gmail.com>
2023-07-12 06:05:20 +0100

    Basic_SQL_Statement First Commit

---
+++ Basic_SQL_Statement.sql
@@ -0,0 +1,64 @@
+
+use classicmodels;
+
+/*what is the total number of customers?*/
+SELECT 
+    COUNT(customerNumber) AS totalCustomers
+FROM
+    customers;
+
+/*what is the total number of employees?*/
+SELECT 
+    COUNT(employeeNumber) AS totalEmployee
+FROM
+    employees;
+
+/*in how many countries does the company have an office?*/
+SELECT 
+    COUNT(DISTINCT (country)) AS totalCountriesWithAnOffice
+FROM
+    offices;
+
+/*list countries where the company has an office and the number of offices present in it?*/
+SELECT 
+    country, COUNT(country) AS offices
+FROM
+    offices
+GROUP BY country
+ORDER BY offices;
+
+/*how many products does the company have?*/
+SELECT 
+    COUNT(DISTINCT (productName)) totalProduct
+FROM
+    products;
+
+/*how many customers are a limited company*/
+SELECT 
+    COUNT(customerNumber) AS totalLtd
+FROM
+    customers
+WHERE
+    customerName LIKE '%Ltd%';
+
+
+/*what is the maximum and minimum credit limit of the customer?*/
+SELECT 
+    MIN(creditLimit) AS minLimit, MAX(creditLimit) AS maxLimit
+FROM
+    customers;
+
+/*how many customers are without a sales representative?*/
+SELECT 
+    COUNT(customerNumber)
+FROM
+    customers
+WHERE
+    salesRepEmployeeNumber IS NULL;
+
+/*what is the average credit limit offered?*/
+SELECT 
+    AVG(creditLimit) AS avgLimit
+FROM
+    customers;
+
